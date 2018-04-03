97208199e5d8f962d04a84c501704d6a99d4819b
sunsilkky@gmail.com <sunsilkky@gmail.com>
2018-04-03 16:02:28 -0400

    added sep_defs

---
+++ src/test/java/com/app/step_definitions/CreatePageStepDefinitions.java
@@ -0,0 +1,47 @@
+package com.app.step_definitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class CreatePageStepDefinitions {
+
+	@Given("^I logged into Mahara as username \"([^\"]*)\" and password \"([^\"]*)\"$")
+	public void i_logged_into_Mahara_as_username_and_password(String arg1, String arg2) {
+
+	}
+
+	@Then("^I click add new page$")
+	public void i_click_add_new_page() {
+
+	}
+
+	@Then("^I enter the page title as \"([^\"]*)\"$")
+	public void i_enter_the_page_title_as(String arg1) {
+
+	}
+
+	@Then("^I enter the page description  as \"([^\"]*)\"$")
+	public void i_enter_the_page_description_as(String arg1) {
+
+	}
+
+	@Then("^I save the page$")
+	public void i_save_the_page() {
+
+	}
+
+	@Then("^I click display page$")
+	public void i_click_display_page() {
+
+	}
+
+	@Then("^I should see page title as \"([^\"]*)\"$")
+	public void i_should_see_page_title_as(String arg1) {
+
+	}
+
+	@Then("^the page description is \"([^\"]*)\"$")
+	public void the_page_description_is(String arg1) {
+
+	}
+}
