d2dcf4952f0dace56acb9a9ded159d30d745f950
sunsilkky@gmail.com <sunsilkky@gmail.com>
2018-04-09 12:56:49 -0400

    fixed method logic

--- src/test/java/com/app/pages/MaharaDashboardPage.java
+++ src/test/java/com/app/pages/MaharaDashboardPage.java
@@ -1,11 +1,15 @@
 package com.app.pages;
 
+import java.util.List;
+
+import org.apache.velocity.tools.view.tools.BrowserSnifferTool;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+import com.app.utilities.BrowserUtils;
 import com.app.utilities.Driver;
 
 public class MaharaDashboardPage {
@@ -27,7 +31,7 @@ public class MaharaDashboardPage {
 	@FindBy(xpath="//li[@class='myportfolio']/button[@class='navbar-showchildren navbar-toggle dropdown-toggle collapsed']")
 	public WebElement portfolio;
 
-	@FindBy(linkText="RÃ©sumÃ©")
+	@FindBy(linkText="Résumé")
 	public WebElement resume;
 	
 	@FindBy(xpath="//a[.='Portfolio']")
@@ -36,24 +40,40 @@ public class MaharaDashboardPage {
 	@FindBy(id = "nav")
 	public WebElement navigationList;
 	
-	@FindBy(xpath="//a[@href=\"http://34.223.219.142:1022/mahara/artefact/blog/index.php\"]")
+	@FindBy(xpath="//a[@href=\"http://34.223.219.142:1022/mahara/artefact/blog/index.php\"]") 
 	public WebElement journals;
+	
+	@FindBy(xpath="//ul[@id='childmenu-1']/li/a")
+	public List<WebElement> contentList;
 
 
 	public void clickOnContectList(String menu) {
+		menu = menu.toLowerCase();
 		contentDropDown.click();
+		BrowserUtils.waitForVisibility(contentList.get(3), 3);
 		
-		switch(menu) {
-		case "Journals":
-			journals.click();
-			break;
-		case "Profile":
-			profile.click();
-			break;
-		case "Résumé":
-			resume.click();
-			break;
+		LOOP:
+		for(int i = 0; i < contentList.size(); i++) {
+			WebElement element = contentList.get(i);
+			String content = element.getText().toLowerCase();
+			
+			System.out.println(contentList.size());
+			System.out.println(content);
+			
+			if (content.equals(menu)) {
+				switch(menu) {
+				case "journals":
+					element.click();
+					break LOOP;
+				case "profile":
+					element.click();
+					break LOOP;
+				}
+				
+			}
 		}
+		
+		
 		
 	}
 	
