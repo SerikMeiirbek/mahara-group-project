b80d4bc139006233a0edcbe09ba58b3d4d8e70a9
sunsilkky@gmail.com <sunsilkky@gmail.com>
2018-04-16 18:38:15 -0400

    implemented new steps

--- src/test/java/com/app/step_definitions/ProfilePageStepDefinitions.java
+++ src/test/java/com/app/step_definitions/ProfilePageStepDefinitions.java
@@ -81,11 +81,97 @@ public class ProfilePageStepDefinitions {
 	@Then("^followings should be displayed$")
 	public void followings_should_be_displayed(List<String> contents) {
 		BrowserUtils.waitForVisibility(profilePage.officialwebsite, 5);
-		
+
 		assertEquals(profilePage.officialwebsite.getText(), contents.get(0));
 		assertEquals(profilePage.personalwebsite.getText(), contents.get(1));
 		assertEquals(profilePage.blogaddress.getText(), contents.get(2));
 		assertEquals(profilePage.postaladdress.getText(), contents.get(3));
+	}
+
+	@Then("^I create contact information:$")
+	public void i_create_contact_information(Map<String, String> details) {
+		profilePage.ContactInformation.click();
+		BrowserUtils.waitForVisibility(profilePage.officialwebsiteF, 5);
+
+		for (String key : details.keySet()) {
+
+			switch (key) {
+			case "Official website address":
+				profilePage.officialwebsiteF.clear();
+				profilePage.officialwebsiteF.sendKeys(details.get(key));
+				break;
+			case "Personal website address":
+				profilePage.personalwebsiteF.clear();
+				profilePage.personalwebsiteF.sendKeys(details.get(key));
+				break;
+			case "Blog address":
+				profilePage.blogaddressF.clear();
+				profilePage.blogaddressF.sendKeys(details.get(key));
+				break;
+			case "Postal address":
+				profilePage.postaladdressF.clear();
+				profilePage.postaladdressF.sendKeys(details.get(key));
+				break;
+			case "Town":
+				profilePage.townF.clear();
+				profilePage.townF.sendKeys(details.get(key));
+				break;
+			}
+
+		}
+	}
+
+	@Then("^I should see following information:$")
+	public void i_should_see_following_information(Map<String, String> details) {
+		BrowserUtils.waitForVisibility(profilePage.officialwebsiteF, 5);
+
+		for (String key : details.keySet()) {
+			
+			switch (key) {
+			case "Official website address":
+				assertEquals(profilePage.officialwebsiteF.getAttribute("value"), details.get(key));
+				break;
+			case "Personal website address":
+				assertEquals(profilePage.personalwebsiteF.getAttribute("value"), details.get(key));
+				break;
+			case "Blog address":
+				assertEquals(profilePage.blogaddressF.getAttribute("value"), details.get(key));;
+				break;
+			case "Postal address":
+				assertEquals(profilePage.postaladdressF.getAttribute("value"), details.get(key));
+				break;
+			case "Town":
+				assertEquals(profilePage.townF.getAttribute("value"), details.get(key));
+				break;
+			}
+		}
+	}
+	
+	
+	@Then("^I click on General$")
+	public void i_click_on_General() {
+	    profilePage.General.click();
+	}
+
+	@Then("^General contents followings should be displayed:$")
+	public void general_contents_followings_should_be_displayed(List<String> contents) {
+		BrowserUtils.waitForVisibility(profilePage.occupation, 5);
+		assertEquals(profilePage.occupation.getText(), contents.get(0));
+		assertEquals(profilePage.industry.getText(), contents.get(1));
+	}
+
+	@Then("^I add required information:$")
+	public void i_add_required_information(Map<String, String> input) {
+		profilePage.occupationF.clear();
+		profilePage.occupationF.sendKeys(input.get("Occupation"));
+		profilePage.industryF.clear();
+		profilePage.industryF.sendKeys(input.get("Industry"));
+	}
+
+	@Then("^following information should be displayed$")
+	public void following_information_should_be_displayed(Map<String, String> input) {
+	    assertEquals(profilePage.occupationF.getAttribute("value"), input.get("Occupation"));
+	    assertEquals(profilePage.industryF.getAttribute("value"), input.get("Industry"));
 	}
 
 }
