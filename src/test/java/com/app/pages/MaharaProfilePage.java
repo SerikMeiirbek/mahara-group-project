6bd2ef2fec74bc434b385e8d8015a9c8d61f50f4
sunsilkky@gmail.com <sunsilkky@gmail.com>
2018-04-16 17:48:36 -0400

    added more webelements

--- src/test/java/com/app/pages/MaharaProfilePage.java
+++ src/test/java/com/app/pages/MaharaProfilePage.java
@@ -28,6 +28,54 @@ public class MaharaProfilePage {
 	@FindBy(id = "messages")
 	public WebElement messages;
 	
+	@FindBy(id = "profileform_firstname")
+	public WebElement firstName;
+	
+	@FindBy(id = "profileform_lastname")
+	public WebElement lastName;
+	
+	@FindBy(id = "profileform_studentid")
+	public WebElement studentID;
+	
+	@FindBy(id = "profileform_preferredname")
+	public WebElement displayName;
+	
+	@FindBy(linkText = "Contact information")
+	public WebElement ContactInformation;
+	
+	@FindBy(xpath = "//div[@id='profileform_officialwebsite_container']/label")
+	public WebElement officialwebsite;
+	
+	@FindBy(xpath = "//div[@id='profileform_personalwebsite_container']/label")
+	public WebElement personalwebsite;
+	
+	@FindBy(xpath = "//div[@id='profileform_blogaddress_container']/label")
+	public WebElement blogaddress;
+	
+	@FindBy(xpath = "//div[@id='profileform_address_container']/label")
+	public WebElement postaladdress;
+	
+	@FindBy(xpath = "//div[@id='profileform_town_container']/label")
+	public WebElement town;
+	
+	@FindBy(xpath = "//div[@id='profileform_city_container']/label")
+	public WebElement city_region;
+	
+	@FindBy(xpath = "//div[@id='profileform_country_container']/label")
+	public WebElement country;
+	
+	@FindBy(xpath = "//div[@id='profileform_homenumber_container']/label")
+	public WebElement homenumber;
+	
+	@FindBy(xpath = "//div[@id='profileform_businessnumber_container']/label")
+	public WebElement businessnumber;
+	
+	@FindBy(xpath = "//div[@id='profileform_mobilenumber_container']/label")
+	public WebElement mobilenumber;
+	
+	@FindBy(xpath = "//div[@id='profileform_faxnumber_container']/label")
+	public WebElement faxnumber;
+	
 	public void addIntroduction(String introduction) {
 		driver.switchTo().frame(iframe);
 		introductionField.clear();
